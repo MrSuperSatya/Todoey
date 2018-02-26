9cf515c3012e56ae4e216cb497190f79c16784b1
Sokunsatya Sangvat <satya@macs-macbook-pro.local>
2018-02-27 03:43:37 +0700

    First commit with data source and delegate methods

---
+++ Todoey/ViewControllerToDoList.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Satya on 2/25/18.
+//  Copyright © 2018 angkorsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerToDoList: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Kill bad guys"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellToDoItem", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+        }
+        else {
+            cell?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
